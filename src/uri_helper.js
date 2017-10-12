9026495eff5405ad1857b5b9f912b4bb771b8989
Shankar Thiyagaraajan <shankarthiyagaraajan@gmail.com>
2017-10-12 14:06:14 +0530

    Update uri_helper.js
    #1 Add & Bulk Add actions improved,
    #2 Remove action improved,
    #3 Overall flow improved.

--- src/uri_helper.js
+++ src/uri_helper.js
@@ -21,7 +21,12 @@
 
 var URI = {
 
-    // To Get Parameter by its Name [ex. ?page=123, function('page') => 123]
+    /**
+     * To Get Parameter by its Name [ex. ?page=123, function('page') => 123]
+     * @name : Name of an item to return,
+     * @url : User defined URL to get item back,
+     * @default_result : If item not found, then return the default value.
+     */
     get: function (name, url, default_result) {
 
         // Sanity check.
@@ -32,10 +37,9 @@ var URI = {
         // Default response.
         var response;
 
-        // Sanity check.
+        // If user defined URI not given, then take active URI.
         if (!url) {
-
-            // If not valid, then update the active url.
+            // Getting active URI.
             url = window.location.href;
         }
 
@@ -53,18 +57,23 @@ var URI = {
         return response;
     },
 
-    // To get an URI data by index.
+    /**
+     * To get all URI params.
+     * @byString : To retrun key & value pair as string.
+     */
     getAll: function (byString) {
-        // Sanity check.
+        // If byString param is not defined, then take false as default.
         if (typeof byString === 'undefined') byString = false;
+        // Initiating variables.
         var queryDict;
+        // Empty object initated.
         queryDict = {};
-
+        // If byString, then retrun string as array.
         if (byString) {
             // Return param list as String.
             return location.search.substr(1).split("&");
         } else {
-            // To loop the params.
+            // Else, then loop to generate the object list of URI params.
             location.search.substr(1).split("&").forEach(function (item) {
                 // If empty, then return.
                 if ('' !== item) {
@@ -74,47 +83,48 @@ var URI = {
                 }
             });
         }
+        // Return all list.
         return queryDict;
     },
-
-    // To clear all URI params.
+    /**
+     * To clear all URI params.
+     */
     clear: function () {
         // To get all URI data's.
         var list = this.getAll();
-
         // Loop to remove all URI data's.
         for (var i in list) {
             this.remove(i, list[i], false);
         }
-
         // Update URI status.
-        this.update();
+        this.updateStructure();
     },
-
-    update: function () {
+    /**
+     * To Trigger general update action for internal purpose.
+     */
+    updateStructure: function () {
         // Get active URL.
         var href = window.location.href;
-
         // Check and remove "?" from URL.
         if (href.indexOf('?') !== -1) {
-
             // Remove "?" from URL.
             href = href.slice(0, href.indexOf('?'));
-
             // Update to URL.
             window.history.pushState('', 'Title', href)
         }
     },
-
-    // To Remove Params by object or single.
+    /**
+     * To Remove Params by object or single.
+     * @list : List of items to remove.
+     */
     remove: function (list, value, multiple) {
-
+      //TODO: check and remove "value" usage.
         // To Get list of Params.
         var core_list = this.getAll();
         // Sanity check.
         if('undefined' === core_list) core_list = 0;
         // If no objects, then clear all data.
-        if (0 === this.objCount(core_list)) return this.update();
+        if (0 === this.objCount(core_list)) return this.updateStructure();
 
         var isObject = true;
 
@@ -155,7 +165,7 @@ var URI = {
             item_out = item.join(',');
             temp_obj[list] = item_out;
             // Update the resulted value value to URI.
-            this.add([temp_obj]);
+            this.add(list, item_out);
         } else {
             if (true === isObject) {
                 // Generate Update List by eliminating the element list.
@@ -181,17 +191,39 @@ var URI = {
             window.history.pushState('', 'Title', newQuery);
         }
     },
-    // To remove all params in URI.
+    /**
+     * To remove all params in URI.
+     * @reset : To Reset with window reload or not.
+     */
     removeAll: function (reset) {
-        var href = window.document.href;
+      //TODO: Remove those are created by this only.
+        var href = window.location.href;
         if (-1 !== href.indexOf('?')) {
             href = href.split('?');
             window.history.pushState('', 'Title', href[0]);
         }
         if (true === reset) window.location.reload();
     },
-    // To Add Param To URI.
-    add: function (list, append) {
+    /**
+    * To Add one param to URL.
+    * @key : name of the item,
+    * @value : value of the item,
+    * @append : Is append value to existing or not.
+    */
+    add: function(key, value, append){
+      // Create empty object.
+      var list = {};
+      // Adding new entry.
+      list[key] = value;
+      // Trigger function to append item.
+      this.bulkAdd([list], append);
+    },
+    /**
+     * To Add multiple params to the URI.
+     * @list : List of items to add to URI.
+     * @append : Is action to append values to existing params or not.
+     */
+    bulkAdd: function (list, append) {
         var temp_object;
         // To Check the type is Object or Not.
         if (typeof list != 'object') return false;
@@ -202,26 +234,36 @@ var URI = {
 
         // Append multiple value for single index.
         if (true === append) {
+          // Initiating variables.
             var val;
             var val_result;
+            // Looping the params list.
             for (var i in list) {
+              // Check each blocks are object or not.
                 if (typeof list[i] === 'object') {
+                  // Getting item by key & value.
                     for (var k in list[i]) {
+                      // Get value of an item, if exists already.
                         val_result = URI.get(k);
+                        // Check is this item is already exist, then
                         if (false !== val_result) {
+                          // Appending value by separator,
                             temp_object = val_result.split(',');
                             // If already exists, then ignore.
                             if (-1 !== temp_object.indexOf(list[i][k])) continue;
                             val = val_result + ',';
                             // If not exist, then reset.
                             if (false === val_result || '' === val_result) {
+                              // If no values are in existing item, then start with empty space.
                                 val = '';
                             }
-                            // Append to the list.
+                            // Append initial value with item's value.
                             val = val + list[i][k];
                         } else {
+                          // Default initial value.
                             val = list[i][k];
                         }
+                        // Load back to params list.
                         updated_list[k] = val;
                     }
                 }
@@ -238,7 +280,6 @@ var URI = {
                 }
             }
         }
-
         var newQuery = '?';
         // To Form New and Updated Query.
         console.log(updated_list);
@@ -249,16 +290,31 @@ var URI = {
         // To Update the URI.
         window.history.pushState('', 'Title', newQuery.slice(0, -1));
     },
+    /**
+     * To record entry for entry management.
+     */
+    recordEntry: function(){
+
+    }
+    /**
+     * General fix for URI.
+     */
     fixURI: function () {
+      // To get all item.
         var URI_items = this.getAll();
+        // Remove all existing URI.
         this.removeAll();
+        // Loop to add back the items.
         for (var i in URI_items) {
             if ('undefined' !== URI_items[i]) {
-                this.add([{i: URI_items[i]}]);
+              // Add back the item to URI.
+              this.add(i, URI_items[i]);
             }
         }
     },
-    // To Go to Next Page.
+    /**
+     * Classic function for pagination purpose to go next page.
+     */
     nextPage: function () {
         // To Get the actual value of "page"
         var page = this.get('page');
@@ -269,7 +325,9 @@ var URI = {
         // Update the Param "page" value.
         this.replaceParam('page', page);
     },
-    // To Go back to Previous Page.
+    /**
+     * Classic function for pagination purpose to go previous page.
+     */
     prevPage: function () {
         // To Get the actual value of "page"
         var page = this.get('page');
@@ -286,72 +344,58 @@ var URI = {
         // Update the Param "page" value.
         this.replaceParam('page', page);
     },
-
-    // Sanity check of param.
+    /**
+     * Check whether the param is exist or not.
+     */
     isParamExists: function (param) {
-
         // To extract the URI from URL.
         var paramString = location.search.substr(1);
-
         // Return, param is exist or not.
         return (paramString.indexOf(param + '=') !== -1);
     },
-
-    // To Replace | Append | Create param. [DEPRECATED]
-    replaceParam: function (param, value) {
-
+    /**
+     * To Update | Append | Create param.
+     */
+    update: function (param, value) {
         // To extract the URI from URL.
         var queryString = location.search.substr(1);
-
         // To Find the Matched element.
         var total = queryString.match(/[a-z\d]+=[a-zA-Z\d]+/gi);
-
         // Form New Param with Value.
         var newValue = param + '=' + value;
-
         // Form Old Param with Value.
         var oldValue = param + '=' + this.get(param);
-
         // To Check have param or not.
         if (this.objCount(total) > 0) {
-
             // To Check, whether param is exist or not.
             if (this.isParamExists(param)) {
-
                 // To Update the Value, If param already exist.
                 queryString = '?' + queryString.replace(oldValue, newValue);
             } else {
-
                 // To Create New Param, If not exist.
                 queryString = '?' + queryString + '&' + newValue;
             }
         } else {
-
             // To Create First param, If no param exist.
             queryString = '?' + newValue;
         }
-
         // To update to the URI.
         window.history.pushState('', 'Title', queryString);
-
         return queryString;
     },
-
-    // To Count number of object elements.
+    /**
+     * To Count number of object elements.
+     */
     objCount: function (object) {
-
         // Initiating Length.
         var length = 0;
-
         // Looping the Objects.
         for (var key in object) {
-
             // Check with object existance.
             if (object.hasOwnProperty(key)) {
                 ++length;
             }
         }
-
         // Total count of Objects.
         return length;
     }
